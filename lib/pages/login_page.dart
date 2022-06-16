5c4914ab9c284a04bd6a4591a730b76ec1aa5ebc
MeetSavaj <meetsavaj10@gmail.com>
2022-06-16 09:37:44 +0530

    First Commit

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -6,13 +6,51 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text(
-          "Login page",
-          style: TextStyle(
-              fontSize: 20, color: Colors.blue, fontWeight: FontWeight.bold),
-        ),
-      ),
-    );
+        color: Colors.white,
+        child: Column(
+          children: [
+            Image.asset(
+              "assets/images/login_page.png",
+              fit: BoxFit.cover,
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Text(
+              "Welcome",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                        hintText: "Enter UserName", labelText: "Username"),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: "Enter Password",
+                      labelText: "Password",
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  ElevatedButton(
+                      onPressed: () {
+                        print("Hi meet");
+                      },
+                      child: Text("Login"))
+                ],
+              ),
+            )
+          ],
+        ));
   }
 }
