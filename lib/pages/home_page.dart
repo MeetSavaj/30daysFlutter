1b4d5ccea8c005aab3006799921b8ee899b0c415
MeetSavaj <meetsavaj10@gmail.com>
2022-06-19 17:00:02 +0530

    First Commit

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -22,30 +22,37 @@ class _HomePageState extends State<HomePage> {
   }
 
   loadData() async {
+    await Future.delayed(Duration(seconds: 2));
     final catalog_json =
         await rootBundle.loadString("assets/files/catalog.json");
     final decodedData = jsonDecode(catalog_json);
     var productsData = decodedData["products"];
+    CatalogModel.items = List.from(productsData)
+        .map<Item>((item) => Item.fromMap(item))
+        .toList();
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
-    final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
-
     return Scaffold(
       appBar: AppBar(
         title: Text("Test_1"),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: ListView.builder(
-          itemCount: dummyList.length,
-          itemBuilder: ((context, index) {
-            return ItemWidget(
-              item: dummyList[index],
-            );
-          }),
-        ),
+        child: (CatalogModel.items != null && CatalogModel.items!.isNotEmpty)
+            ? ListView.builder(
+                itemCount: CatalogModel.items!.length,
+                itemBuilder: ((context, index) => ItemWidget(
+                      item: CatalogModel.items![index],
+                    )),
+              )
+            : Center(
+                child: CircularProgressIndicator(
+                  color: Colors.deepPurple,
+                ),
+              ),
       ),
       drawer: MyDrawer(),
     );
