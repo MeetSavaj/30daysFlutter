d05bddbc9ac7f92e62737698170c0d22f171417c
MeetSavaj <meetsavaj10@gmail.com>
2022-06-24 11:35:56 +0530

    First Commit

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,24 +1,27 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
-import 'dart:convert';
 import 'package:flutter/cupertino.dart';
-import "package:flutter/material.dart";
-import 'package:flutter/services.dart';
+import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:test_1/core/store.dart';
+import 'package:test_1/models/cart.dart';
 import 'package:test_1/models/catalog.dart';
 import 'package:test_1/utils/routes.dart';
 import 'package:test_1/widgets/home_widgets/catalog_header.dart';
 import 'package:test_1/widgets/home_widgets/catalog_list.dart';
-import 'package:test_1/widgets/themes.dart';
-import "package:velocity_x/velocity_x.dart";
+import 'package:velocity_x/velocity_x.dart';
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatefulWidget {
   @override
-  State<HomePage> createState() => _HomePageState();
+  _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
   final int days = 30;
 
-  final String name = "codepur";
+  final String name = "Codepur";
+
+  final url =
+      "https://api.jsonbin.io/b/604dbddb683e7e079c4eefd3"; //link not working, we can take different link!!
 
   @override
   void initState() {
@@ -28,9 +31,12 @@ class _HomePageState extends State<HomePage> {
 
   loadData() async {
     await Future.delayed(Duration(seconds: 2));
-    final catalog_json =
-        await rootBundle.loadString("assets/files/catalog.json");
-    final decodedData = jsonDecode(catalog_json);
+    // final catalogJson =
+    //     await rootBundle.loadString("assets/files/catalog.json");
+
+    final response = await http.get(Uri.parse(url));
+    final catalogJson = response.body;
+    final decodedData = jsonDecode(catalogJson);
     var productsData = decodedData["products"];
     CatalogModel.items = List.from(productsData)
         .map<Item>((item) => Item.fromMap(item))
@@ -40,31 +46,42 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
+    final _cart = (VxState.store as MyStore).cart;
     return Scaffold(
-      backgroundColor: context.canvasColor,
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => Navigator.pushNamed(context, MyRoutes.cartRoute),
-        backgroundColor: context.theme.buttonColor,
-        child: Icon(
-          CupertinoIcons.cart,
-          color: Colors.white,
+        backgroundColor: context.canvasColor,
+        floatingActionButton: VxBuilder(
+          mutations: {AddMutation, RemoveMutation},
+          builder: (ctx, _, status) => FloatingActionButton(
+            onPressed: () => Navigator.pushNamed(context, MyRoutes.cartRoute),
+            backgroundColor: context.theme.buttonColor,
+            child: Icon(
+              CupertinoIcons.cart,
+              color: Colors.white,
+            ),
+          ).badge(
+              color: Vx.gray200,
+              size: 22,
+              count: _cart.items.length,
+              textStyle: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+              )),
         ),
-      ),
-      body: SafeArea(
-        child: Container(
-          padding: Vx.m32,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              CatalogHeader(),
-              if (CatalogModel.items != null && CatalogModel.items!.isNotEmpty)
-                CatalogList().py16().expand()
-              else
-                CircularProgressIndicator().centered().expand(),
-            ],
+        body: SafeArea(
+          child: Container(
+            padding: Vx.m32,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CatalogHeader(),
+                if (CatalogModel.items != null &&
+                    CatalogModel.items!.isNotEmpty)
+                  CatalogList().py16().expand()
+                else
+                  CircularProgressIndicator().centered().expand(),
+              ],
+            ),
           ),
-        ),
-      ),
-    );
+        ));
   }
 }
