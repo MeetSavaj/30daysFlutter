ff6552ea08cc77e7e456c83d385af1944832bcce
MeetSavaj <meetsavaj10@gmail.com>
2022-06-24 09:55:40 +0530

    First Commit

--- lib/pages/home_detail_page.dart
+++ lib/pages/home_detail_page.dart
@@ -1,19 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:test_1/models/catalog.dart';
-import 'package:test_1/widgets/themes.dart';
+import 'package:test_1/widgets/home_widgets/add_to_cart.dart';
 import 'package:velocity_x/velocity_x.dart';
 
 class HomeDetailPage extends StatelessWidget {
+  final Item catalog;
+
   const HomeDetailPage({Key? key, required this.catalog})
       : assert(catalog != null),
         super(key: key);
-  final Item catalog;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-      ),
+      appBar: AppBar(backgroundColor: Colors.transparent),
       backgroundColor: context.canvasColor,
       bottomNavigationBar: Container(
         color: context.cardColor,
@@ -22,16 +21,9 @@ class HomeDetailPage extends StatelessWidget {
           buttonPadding: EdgeInsets.zero,
           children: [
             "\$${catalog.price}".text.bold.xl4.red800.make(),
-            ElevatedButton(
-                    onPressed: () {},
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all(
-                        context.theme.buttonColor,
-                      ),
-                      shape: MaterialStateProperty.all(StadiumBorder()),
-                    ),
-                    child: "Add to cart".text.make())
-                .wh(120, 50)
+            AddToCart(
+              catalog: catalog,
+            ).wh(120, 50)
           ],
         ).p32(),
       ),
@@ -40,36 +32,34 @@ class HomeDetailPage extends StatelessWidget {
         child: Column(
           children: [
             Hero(
-                    tag: Key(catalog.id.toString()),
-                    child: Image.network(catalog.image))
-                .h32(context),
+              tag: Key(catalog.id.toString()),
+              child: Image.network(catalog.image),
+            ).h32(context),
             Expanded(
                 child: VxArc(
-                    height: 30.0,
-                    arcType: VxArcType.CONVEY,
-                    edge: VxEdge.TOP,
-                    child: Container(
-                      width: context.screenWidth,
-                      color: context.cardColor,
-                      child: Column(
-                        children: [
-                          catalog.name.text.xl4
-                              .color(context.accentColor)
-                              .bold
-                              .make(),
-                          catalog.desc.text
-                              .textStyle(context.captionStyle)
-                              .xl
-                              .make(),
-                          10.heightBox,
-                          "Eos aliquyam vero voluptua rebum dolor tempor sed. Rebum ipsum et est ipsum ut sadipscing. Et sit sea est lorem sit vero sadipscing et. Et sadipscing dolores invidunt et tempor. Ipsum no labore sit ut, amet et eos sanctus dolore dolores voluptua. Labore erat dolor diam invidunt consetetur magna takimata.."
-                              .text
-                              .textStyle(context.captionStyle)
-                              .make()
-                              .p16()
-                        ],
-                      ).py64(),
-                    ))),
+              height: 30.0,
+              arcType: VxArcType.CONVEY,
+              edge: VxEdge.TOP,
+              child: Container(
+                color: context.cardColor,
+                width: context.screenWidth,
+                child: Column(
+                  children: [
+                    catalog.name.text.xl4
+                        .color(context.accentColor)
+                        .bold
+                        .make(),
+                    catalog.desc.text.textStyle(context.captionStyle).xl.make(),
+                    10.heightBox,
+                    "Dolor sea takimata ipsum sea eirmod aliquyam est. Eos ipsum voluptua eirmod elitr, no dolor dolor amet eirmod dolor labore dolores magna. Amet vero vero vero kasd, dolore sea sed sit invidunt nonumy est sit clita. Diam aliquyam amet tempor diam no aliquyam invidunt. Elitr lorem eirmod dolore clita. Rebum."
+                        .text
+                        .textStyle(context.captionStyle)
+                        .make()
+                        .p16()
+                  ],
+                ).py64(),
+              ),
+            ))
           ],
         ),
       ),
--- lib/widgets/home_widgets/catalog_list.dart
+++ lib/widgets/home_widgets/catalog_list.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:test_1/models/cart.dart';
 import 'package:test_1/models/catalog.dart';
 import 'package:test_1/pages/home_detail_page.dart';
 import 'package:test_1/widgets/home_widgets/catalog.image.dart';
 import 'package:velocity_x/velocity_x.dart';
+import 'add_to_cart.dart';
 
 class CatalogList extends StatelessWidget {
+  const CatalogList({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
@@ -22,7 +24,6 @@ class CatalogList extends StatelessWidget {
           ),
           child: CatalogItem(
             catalog: catalog,
-            key: null,
           ),
         );
       },
@@ -61,7 +62,7 @@ class CatalogItem extends StatelessWidget {
                 buttonPadding: EdgeInsets.zero,
                 children: [
                   "\$${catalog.price}".text.bold.xl.make(),
-                  _AddToCart(catalog: catalog)
+                  AddToCart(catalog: catalog)
                 ],
               ).pOnly(right: 8.0)
             ],
@@ -69,40 +70,5 @@ class CatalogItem extends StatelessWidget {
         ],
       ),
     ).color(context.cardColor).rounded.square(150).make().py16();
-  }
-}
-
-class _AddToCart extends StatefulWidget {
-  final Item catalog;
-  const _AddToCart({
-    Key? key,
-    required this.catalog,
-  }) : super(key: key);
-
-  @override
-  __AddToCartState createState() => __AddToCartState();
-}
-
-class __AddToCartState extends State<_AddToCart> {
-  bool isAdded = false;
-
-  @override
-  Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () {
-        isAdded = isAdded.toggle();
-        final _catalog = CatalogModel();
-        final _cart = CartModel();
-        _cart.catalog = _catalog;
-        _cart.add(widget.catalog);
-        setState(() {});
-      },
-      style: ButtonStyle(
-          backgroundColor: MaterialStateProperty.all(context.theme.buttonColor),
-          shape: MaterialStateProperty.all(
-            StadiumBorder(),
-          )),
-      child: isAdded ? Icon(Icons.done) : "Add to cart".text.make(),
-    );
   }
 }
